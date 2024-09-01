4a8c4dd8650ac7df2540d982395775e18f88c5f1
arianashafiee <143358685+arianashafiee@users.noreply.github.com>
2024-09-01 13:36:55 -0400

    added parentheses

--- csf_assign01/bigint.cpp
+++ csf_assign01/bigint.cpp
@@ -208,7 +208,7 @@ std::string BigInt::to_hex() const {
 
 
 bool BigInt::is_zero() const {
-    return bits.empty() || bits.size() == 1 && bits[0] == 0;
+    return bits.empty() || (bits.size() == 1 && bits[0] == 0);
 }
 
 static int compare_magnitudes(const BigInt &lhs, const BigInt &rhs) {
