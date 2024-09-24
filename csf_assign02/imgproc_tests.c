9f05d3b101dff6e58293769c8e291bfa73d820be
arianashafiee <143358685+arianashafiee@users.noreply.github.com>
2024-09-23 22:28:48 -0400

    .

--- csf_assign02/imgproc_tests.c
+++ csf_assign02/imgproc_tests.c
@@ -601,9 +601,6 @@ void test_copy_tile(TestObjs *objs) {
     destroy_img(actual_tile_image);
 }
 
-
-
-
 void test_get_r(TestObjs *objs) {
   // Test 1: Basic case, arbitrary red value
   uint32_t pixel = 0x7F000000; // Red component = 127 (0x7F), other components = 0
